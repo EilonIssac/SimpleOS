2030efc0fa24817f29bdb0219e6e5f28969e20cb
Eilon Adelson <eilonadelson@me.com>
2024-05-12 23:57:29 -0400

    Install script finished Minor config changes to xmenu and xbindkeysrc All paths now dynamic Added xlaunch to bin

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ while read -r package; do
 	sudo pacman -S --needed --noconfirm $package || echo "Failed to install $package"
 	yay -Sy --needed --noconfirm --answerdiff=None --answeredit=None $package || echo "Failed to install $package"
 done < pkg.list
+
+sudo cp -r root/etc /etc
+sudo cp -r usr/bin /usr/bin
