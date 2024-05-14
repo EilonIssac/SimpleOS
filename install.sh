75382183985f222d46426141045cae520951d3b6
Eilon Adelson <eilonadelson@me.com>
2024-05-13 22:10:11 -0400

    Changes to installer

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ while read -r package; do
 	yay -Sy --needed --noconfirm --answerdiff=None --answeredit=None $package || echo "Failed to install $package"
 done < pkg.list
 
-chmod -R 555 root/etc
-chmod -R 555 usr/bin
+sudo chmod -R 555 root/etc
+sudo chmod -R 555 usr/bin
 
 sudo rsync -av root/etc /etc
 sudo rsync -av usr/bin /usr/bin
