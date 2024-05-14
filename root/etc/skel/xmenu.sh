50430b849296de9b5067ac5045721f48ad46add9
Eilon Adelson <eilonadelson@me.com>
2024-05-13 20:51:29 -0400

    Minor updates to pkg.list and xmenu

--- root/etc/skel/xmenu.sh
+++ root/etc/skel/xmenu.sh
@@ -6,7 +6,7 @@ IMG:./xmenu_icons/terminal.png	Terminals
 	IMG:./xmenu_icons/Alacritty_logo.svg	Alacritty	alacritty
 	IMG:./xmenu_icons/xterm.svg	XTerm	xterm
 IMG:./xmenu_icons/web.png	Web Browsers
-	IMG:./xmenu_icons/chrome.png	Google Chrome	google-chrome-stable
+	IMG:./xmenu_icons/firefox.svg	Firefox	firefox
 IMG:./xmenu_icons/file_manager.png	File Managment
 	IMG:./xmenu_icons/nautilus.svg	Nautilus	nautilus -w
 IMG:./xmenu_icons/text_editor.png	Text Editing
